80cf65c555b839785eddd48c5cc969cd705f8f09
Fraser Raney <fraserraney@trentu.ca>
2018-01-29 00:49:18 -0500

    input validation

--- Assignment1/src/fiboSeriesRec.java
+++ Assignment1/src/fiboSeriesRec.java
@@ -1,3 +1,4 @@
+import java.util.Scanner;
 
 public class fiboSeriesRec {
 
@@ -6,7 +7,21 @@ public class fiboSeriesRec {
 	}
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Scanner s = new Scanner(System.in);
+		int s0 = 0;
+		while (s0 < 1) {
+			System.out.print("Input a positive integer number, n, for the Fibonacci series to n: ");
+			try {
+				s0 = s.nextInt();
+				if (s0 < 1){
+					System.out.println("Error: Enter a positive integer."); 
+				}
+			} catch (java.util.InputMismatchException e) {
+				System.out.println("Error: Invalid input\nEnter a positive integer.");
+				s.next(); //https://stackoverflow.com/questions/3572160/how-to-handle-infinite-loop-caused-by-invalid-input-inputmismatchexception-usi
+			}
+		}
+		System.out.println("answer: " + s0);
 
 	}
 
